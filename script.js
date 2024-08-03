09cb387956a53e669fba20f7a54cf4533e8f8681
mohd-osman <omohd7584@gmail.com>
2024-08-03 01:00:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
